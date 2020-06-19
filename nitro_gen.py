bf335d6d00837c181d37522812761d573d8df1ec
Vazity <58231563+Vazity1337@users.noreply.github.com>
2020-06-19 17:52:58 -0400

    Update nitro_gen.py

--- nitro_gen.py
+++ nitro_gen.py
@@ -6,8 +6,8 @@ amount = int(input("How many nitro codes do you want to generate\n> "))
 value = 1
 while value <= amount:
     code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
-    file = open('Generated_Nitro.txt', "a")
-    file.write(code + '\n')
-    file.close()
+    f = open('Generated_Nitro.txt', "a")
+    f.write(code + '\n')
+    f.close()
     print(code)
     value += 1
